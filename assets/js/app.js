bfa34ccac3a62bf1baba2372e8d8363a987e8428
Yasir Ali <sdr.yasir@gmail.com>
2025-01-20 15:11:29 +0500

    gordan demo site

---
+++ assets/js/app.js
@@ -0,0 +1,37 @@
+
+
+// let a = prompt('Enetr 1st number');
+// let b = prompt('Enter 2nd number');
+
+// a = parseInt(a);
+// b = parseInt(b);
+
+
+// console.log(a+b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
